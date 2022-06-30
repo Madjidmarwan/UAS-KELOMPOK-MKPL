5e687e58c63681c7192790316de046f2a7359016
aaldishantuy <55171521+aaldishantuy@users.noreply.github.com>
2022-06-30 22:26:13 +0700

    Update Test.java

--- BMI-CALCULATOR-master/BMIcalculator/src/main/java/com/mdhasan/Test.java
+++ BMI-CALCULATOR-master/BMIcalculator/src/main/java/com/mdhasan/Test.java
@@ -1,42 +1,17 @@
 package src.test.java;
 
 import static org.junit.Assert.*;
-import src.main.java.*;
+import src.FXMLController.java.*;
 import org.junit.Test;
 
-public class CounterTest {
+public class AgeTest {
 
 	@Test
-	public void testReset() {
-		Counter testCounter = new Counter();
+	public void testAge() {
+		calculate AgeTest = new calculate();
 		
-		for(int i = 0; i <10; i++) {
-			testCounter.increment();
-		}
+		assertEquals(testAge.getAge(), 9);
 		
-		testCounter.reset();
-		
-		assertEquals(testCounter.getCount(), 0);
-	}
-
-	@Test
-	public void testIncrement() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
-			testCounter.increment();
-			assertEquals(testCounter.getCount(), i);
-		}
-	}
-
-	@Test
-	public void testDecrement() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
-			testCounter.decrement();
-			assertEquals(testCounter.getCount(), i * -1);
-		}
 	}
 
 }
